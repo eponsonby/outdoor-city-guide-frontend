d2d823d85b60073965b68691535fb2cbecbffca2
Erin Ponsonby <erinponsonby@gmail.com>
2020-02-14 14:52:16 -0500

    add handleOnChange and handleOnSubmit function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,70 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { updateLoginForm } from '../actions/loginForm'
 import { connect } from 'react-redux'
 
-const Login = (props) => {
-    return (
-        <form onSubmit={undefined}>
-            <input placeholder="username" value={props.username} name="username" type="text" onChange={undefined} />
-            <input placeholder="password" value={props.password} name="password" type="text" onChange={undefined} />
-            <input type="submit" value="Log In"/>
-        </form>
-    )
-}
+class Login extends Component {
+    state = {
+        username: "",
+        password: ""
+    }
 
-const mapStateToProps = state => {
-    return {
-        username: state.loginForm.username,
-        password: state.loginForm.password
+    handleOnChange = event => {
+        if (event.target.name === "username")
+            this.setState({
+                username: event.target.value,
+            })
+        else if (event.target.name === "password")
+            this.setState({
+                password: event.target.value
+            })
+    }
+
+    handleOnSubmit = event => {
+        event.preventDefault()
+        this.props.setCurrentUser(this.state.username, this.state.password)
+        this.setState({
+            username: "",
+            password: ""
+        })
+    }
+
+    render() {
+        return (
+            <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                <input
+                placeholder="username"
+                value={this.state.username}
+                name="username"
+                type="text"
+                onChange={(event) => this.handleOnChange(event)}
+                />
+
+                <input
+                placeholder="password"
+                value={this.state.password}
+                name="password"
+                type="text"
+                onChange={(event) => this.handleOnChange(event)}
+                />
+
+                <input type="submit" value="Log In"/>
+            </form>
+        )
     }
 }
 
-const mapDispatchToProps = dispatch => {
+    const mapDispatchToProps = (dispatch) => {
+        return (
+            {
+                setCurrentUser: (user) => {
+                    dispatch({
+                        type: 'SET_CURRENT_USER',
+                        user: user
+                    })
+                }
+            }
 
-}
+        )
+    }
 
-export default connect(mapStateToProps, mapDispatchToProps)(Login)
+export default connect (null, mapDispatchToProps)(Login)
