c614852404c962dfa8697a97f5f84fd89d4dabc3
Erin Ponsonby <erinponsonby@gmail.com>
2020-02-14 13:29:27 -0500

    Render login form in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from './components/Login'
 
 class App extends React.Component {
   render() {
   return (
     <div>
-    "Hello"
+      <Login/>
     </div>
   );
 }
