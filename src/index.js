d7bb5d7952b3b883fd4db2519e7a007a18451ead
Erin Ponsonby <erinponsonby@gmail.com>
2020-02-14 12:11:37 -0500

    Create user reducer and action

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
-import thunk from 'redux-thunk'
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import store from './store'
 
-const usersReducer = () => []
-const reducer = combineReducers({
-    usersReducer
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(<Provider store={ store }><App /></Provider>, document.getElementById('root'));
 
