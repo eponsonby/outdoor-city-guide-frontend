3bb53f7abede3c53511b9242be55478eebeb4a01
Erin Ponsonby <erinponsonby@gmail.com>
2020-02-18 15:52:18 -0500

    Update map state to props

--- src/App.js
+++ src/App.js
@@ -9,16 +9,17 @@ class App extends React.Component {
   componentDidMount() {
     this.props.getCurrentUser()
   }
+
   render() {
     return (
       this.props.currentUser ? <Logout/> : <Login/>
-  );
-}
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
   return {
-    currentUser: state
+    currentUser: state.currentUser
   }
 }
 
