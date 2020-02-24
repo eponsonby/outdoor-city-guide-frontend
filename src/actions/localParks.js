44987f6cc4cba16240a3d07e10b9340236d3c04f
Erin Ponsonby <erinponsonby@gmail.com>
2020-02-24 15:42:42 -0500

    Create park urls for fetch

--- src/actions/localParks.js
+++ src/actions/localParks.js
@@ -5,38 +5,56 @@ export const setLocalParks = localParks => {
     }
 }
 
-
-
-
-  export const getLocalParks = (city) => {
-    const getParkIds = () => {
-        switch (city) {
-        case "Boulder":
-            return {type: "recareas", ids: "1106"}
-        case "Reno":
-            return {type: "facilities", ids: ["239692", "239747"]}
-        case "Portland":
-            return {type: "recareas", ids: ["1106", "1102"]}
-        case "Asheville":
-            return [
-                {type: "facilities", ids: ["264802", "240530"]},
-                {type: "recareas", ids: ["2593"]}
-            ]
-        case "Salt Lake City":
-            return [
-                {type: "facilities", ids: ["236556", "240530"]},
-                {type: "recareas", ids: ["1035", "14748"]}
-            ]
-        default:
-            return "Nothing"
-        }
+const getParkTypeAndIds = (city) => {
+    switch (city) {
+    case "Boulder":
+        return [{type: "recareas", ids: ["1106"]}]
+    case "Reno":
+        return [{type: "facilities", ids: ["239692", "239747"]}]
+    case "Portland":
+        return [{type: "recareas", ids: ["1106", "1102"]}]
+    case "Asheville":
+        return [
+            {type: "facilities", ids: ["264802", "240530"]},
+            {type: "recareas", ids: ["2593"]}
+        ]
+    case "Salt Lake City":
+        return [
+            {type: "facilities", ids: ["236556", "240530"]},
+            {type: "recareas", ids: ["1035", "14748"]}
+        ]
+    default:
+        return "Error in Get Local Parks Action"
+    }
 }
 
-const parkIds = getParkIds()
-const test = parkIds.map(id => id)
-console.log(test)
-    return async dispatch => {
+
+const getUrls = (data) => {
+    let urlsArray = []
+    for (let element of data) {
+        if (element.type === "facilities") {
+                for (let ids of element.ids)
+                 urlsArray.push(`http://localhost:3001/api/v1/get_local_parks/?type=facilities?id=${ids}`)
+        }
         
+        if (element.type === "recareas") {
+            for (let ids of element.ids)
+                urlsArray.push(`http://localhost:3001/api/v1/get_local_parks/?type=recareas?id=${ids}`)
+            }   
+    }
+    return urlsArray
+}
+
+// const getIds = (data) => {
+//     for (let element of data) {
+
+//     }
+
+  export const getLocalParks = (city) => {
+      const parkData = getParkTypeAndIds(city)
+      console.log(getUrls(parkData))
+
+    return async dispatch => {
         try {
             const res = await fetch('http://localhost:3001/api/v1/get_local_parks/?id=1106')
             if (!res.ok) {
