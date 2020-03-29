d10954b4904f62e1a3218ef21b859623fa619336
Erin Ponsonby <erinponsonby@gmail.com>
2020-03-29 13:27:59 -0400

    Remove yarn.lock

--- src/components/CitiesHomePage.js
+++ src/components/CitiesHomePage.js
@@ -4,7 +4,35 @@ class CitiesHomePage extends React.Component {
 
     render() {
         return (
-        <div>This is the cities home page</div>
+        <div>
+            <div className="card" style={{width: "18rem"}}>
+                <img src="..." className="card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title">Parks</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">See Parks</a>
+                    </div>
+            </div>
+
+            <div className="card" style={{width: "18rem"}}>
+                <img src="..." className="card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title">Outdoor Stores</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">See Parks</a>
+                    </div>
+            </div>
+
+            <div className="card" style={{width: "18rem"}}>
+                <img src="..." className="card-img-top" alt="..."></img>
+                    <div className="card-body">
+                        <h5 className="card-title">Climbing Gyms</h5>
+                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a href="#" className="btn btn-primary">See Parks</a>
+                    </div>
+            </div>
+        </div>
+
         )
     }
 
