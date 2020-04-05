ffd3793e12a732abc131e7d4aece42fb4363897b
Erin Ponsonby <erinponsonby@gmail.com>
2020-04-05 16:49:14 -0400

    Add text to cards

--- src/components/CitiesHomePage.js
+++ src/components/CitiesHomePage.js
@@ -10,7 +10,7 @@ class CitiesHomePage extends React.Component {
                 <img src='/nationalpark.jpg' className="card-img-top" alt="national park"></img>
                     <div className="card-body">
                         <h5 className="card-title">Parks</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text">Ready to spend some quality time outside? Explore nearby local and national parks. </p>
                         <a href="#" className="btn btn-primary">See Parks</a>
                     </div>
             </div>
@@ -19,8 +19,8 @@ class CitiesHomePage extends React.Component {
                 <img src="/outdoor-stores.jpg" className="card-img-top" alt="..."></img>
                     <div className="card-body">
                         <h5 className="card-title">Outdoor Stores</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" className="btn btn-primary">See Outdoor Store</a>
+                        <p className="card-text">Get geared up at one of the local outdoor shops.</p>
+                        <a href="#" className="btn btn-primary">See Outdoor Stores</a>
                     </div>
             </div>
 
@@ -28,7 +28,7 @@ class CitiesHomePage extends React.Component {
                 <img src="/climbing-gym.jpg" className="card-img-top" alt="..."></img>
                     <div className="card-body">
                         <h5 className="card-title">Climbing Gyms</h5>
-                        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p className="card-text">Can't get to the crag? Visit a nearby indoor climbing gym.</p>
                         <a href="#" className="btn btn-primary">See Climbing Gyms</a>
                     </div>
             </div>
